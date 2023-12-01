762cc597992982177694fe63ef1dd60aa7f75db9
sahil13082003 <sahilgolhar7@gmail.com>
2023-12-01 08:40:39 +0530

    changes Done

---
+++ NOTIFICATION/test.php
@@ -0,0 +1,32 @@
+<?php
+include('smtp/PHPMailerAutoload.php');
+
+echo smtp_mailer('sahilgolhar7@gmail.com','Payment Successfully Done','Your payment has been done successfully. Kindly check your login.');
+function smtp_mailer($to,$subject, $msg){
+	$mail = new PHPMailer(); 
+	$mail->IsSMTP(); 
+	$mail->SMTPAuth = true; 
+	$mail->SMTPSecure = 'tls'; 
+	$mail->Host = "smtp.gmail.com";
+	$mail->Port = 587; 
+	$mail->IsHTML(true);
+	$mail->CharSet = 'UTF-8';
+	//$mail->SMTPDebug = 2; 
+	$mail->Username = "agrirenthub@gmail.com";
+	$mail->Password = "yrmc grym ueod jelw";
+	$mail->SetFrom("email");
+	$mail->Subject = $subject;
+	$mail->Body =$msg;
+	$mail->AddAddress($to);
+	$mail->SMTPOptions=array('ssl'=>array(
+		'verify_peer'=>false,
+		'verify_peer_name'=>false,
+		'allow_self_signed'=>false
+	));
+	if(!$mail->Send()){
+		echo $mail->ErrorInfo;
+	}else{
+		return 'Sent';
+	}
+}
+?>
